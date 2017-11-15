4b1c32ff5d407d7805dc8b1e9b91a24e1e28bc1c
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-15 12:44:18 -0500

    Create top.php

---
+++ top.php
@@ -0,0 +1,19 @@
+<?php
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+$path_parts = pathinfo($phpSelf);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Shred VT</title>
+        
+        <meta charset="UTF-8">
+        <meta name="Author" content="Ben Wasser and Bo Warren">
+        <meta name="Description" content="news, statistics, and images of vermont skiing and snowboarding">
+        <meta name="viewport" content="width=devicew"
+              
+        
+    </head>
+</html>

765d67cd6499259b25bb5daab639640ba56f6b1f
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-16 09:25:11 -0500

    Update top.php

--- top.php
+++ top.php
@@ -18,3 +18,59 @@ $path_parts = pathinfo($phpSelf);
               
         
     </head>
+    <body>
+        <h1>Shred VT</h1>
+        <nav>
+            <ol>
+                <?php
+                print '<li class="';
+                if ($path_parts['filename'] == "home") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="home.php">Home</a>';
+                print '</li>';
+
+                print '<li class="';
+                if ($path_parts['filename'] == "news") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="news.php">News</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "resorts") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="resorts.php">Resorts</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "conditions") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="conditions.php">Conditions</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "media") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="media.php">Media</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "contact") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="contact.php">Contact</a>';
+                print '</li>';
+        
+                ?>
+            </ol>
+        </nav>

9a11aa365807214d221b7b859848b8034403cd94
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-19 15:29:26 -0500

    Update home.php

--- home.php
+++ home.php
@@ -4,3 +4,26 @@ include 'top.php';
 
   </body>
 </html>
+<?php
+    include ('Final Project/top.php');
+?>
+
+<article>
+    <body id="home">
+        <header>
+            <h1>Welcome to ShredVT!</h1>
+        </header>
+        
+    <article id="content">
+        <p>Welcome to ShredVT, the ideal space for Vermont skiers and riders to 
+            come and explore the goings on in the Vermont snow sports community. 
+            On ShredVT, you can read news updates about the various mountains, 
+            check up on the conditions, compare prices, and check out what other 
+            skiers and riders are up to.</p>
+        
+        <figure class="center-medium">
+            <img alt="" src="Final Project/Images/boarder-starr.jpg"
+        </figure>
+        
+</article>
+    </body>

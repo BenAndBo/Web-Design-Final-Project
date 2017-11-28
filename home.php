18f2b2289692b6bc55fe32968de5571f624fc381
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-28 15:42:12 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,5 @@
 <?php
-include 'top.php';
-?>
-
-  </body>
-</html>
-<?php
-    include ('Final Project/top.php');
+    include ('Ftop.php');
 ?>
 
 <article>
@@ -25,5 +19,33 @@ include 'top.php';
             <img alt="" src="Final Project/Images/boarder-starr.jpg"
         </figure>
         
+        <h1>History of Skiing in Vermont</h1>
+        
+        <p>Since the early 20’th century, skiing has been a part of Vermont 
+            culture. Back in 1934, a tow rope powered by a Model-T Ford was set 
+            up on a slope on Clint Gilbert’s farm in Woodstock. This primitive 
+            lift ushered in a new era of skiing to the entire United States, 
+            being the first device of its kind to be solely used to transport 
+            skiers up the hill. In the 1920’s, Vermont Forester Perry Merrill 
+            received permission from the state of Vermont to cut the first 
+            purpose-built ski trails and construct a lodge at Stowe’s Mount 
+            Mansfield. In 1939, the state authorized the lease of the land on 
+            Mount Mansfield to private developers to allow them to construct a 
+            ski resort. While it is ambiguous which mountain can claim the 
+            title of  the first ‘real’ ski resort in the US, Stowe makes a very 
+            good case for itself. By the 1940-1941 season, the longest chair 
+            lift in the world had been constructed at stowe, stretching a full 
+            mile up the slope and further revolutionizing ski culture. Soon ski 
+            resorts began to pop up all over the state and before long, Vermont 
+            became one of the nation’s biggest skiing hotspots. Fast forward to 
+            the 1980’s when a man in living in Londonderry Vermont named Jake 
+            Burton Carpenter began modifying Snurfers, a popular toy resembling 
+            a snowboard without bindings, to have a metal edge like skis and 
+            bindings to lock your feet in place. Carpenter is credited with the 
+            invention of the modern-day snowboard, and went on to create Burton 
+            Snowboards, now the largest snowboarding company in the world.</p>     
+        
+            
+            
 </article>
     </body>

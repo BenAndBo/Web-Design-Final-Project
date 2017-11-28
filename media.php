1026d762a8ec34ab99450ef1bd7f7e66e6592ceb
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-28 16:37:00 -0500

    Update media.php

--- media.php
+++ media.php
@@ -1,6 +1,20 @@
 <?php
-include 'top.php';
+include 'Ftop.php';
 ?>
-
-  </body>
+        <h2>Vermont Media</h2>
+        <table>
+            <tr>
+                <td>
+                    <video width="320" height="240" controls>
+                        <source src="media/Skiing 2016-2017.mp4" type="video/mp4">
+                    </video>
+                    <br>A skiing video from VT.
+                </td>
+                <td>
+                    <iframe src="https://player.vimeo.com/video/117208012" width="320" height="240" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <br>Bush League: Second Inning from Sugarbush Parks
+                </td>
+            </tr>
+        </table>
+    </body>
 </html>

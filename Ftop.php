b2564c58faef346586577a7bb42f28ff8f3dbe8b
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-11-28 15:51:07 -0500

    Update Ftop.php

--- Ftop.php
+++ Ftop.php
@@ -1,8 +1,6 @@
 <?php
 $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
-
 $path_parts = pathinfo($phpSelf);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,63 +12,41 @@ $path_parts = pathinfo($phpSelf);
         <meta name="Description" content="news, statistics, and images of vermont skiing and snowboarding">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="stylesheet" href="final.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="Final.css" type="text/css" media="screen">
               
         
-    </head>
-    <body>
-        <h1>Shred VT</h1>
-        <nav>
-            <ol>
-                <?php
-                print '<li class="';
-                if ($path_parts['filename'] == "home") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="home.php">Home</a>';
-                print '</li>';
+  <?php
+  $debug = false;
+          if(isset($_GET["debug"])) {
+              $debug = true;
+          }
+          
+ 
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%          
+          
+          $domain = "//";
+          
+          $server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, 'UTF-8');
+          
+          $domain .= $server;
+          
+          
+            if ($debug) {
+                print '<p>php Self: ' . $phpSelf;
+                print '<p>Path Parts<pre>';
+                print_r($path_parts);
+                print '</pre></p>';
+            }
+?>
 
-                print '<li class="';
-                if ($path_parts['filename'] == "news") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="news.php">News</a>';
-                print '</li>';
-        
-                print '<li class="';
-                if ($path_parts['filename'] == "resorts") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="resorts.php">Resorts</a>';
-                print '</li>';
-        
-                print '<li class="';
-                if ($path_parts['filename'] == "conditions") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="conditions.php">Conditions</a>';
-                print '</li>';
-        
-                print '<li class="';
-                if ($path_parts['filename'] == "media") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="media.php">Media</a>';
-                print '</li>';
-        
-                print '<li class="';
-                if ($path_parts['filename'] == "contact") {
-                    print ' activePage ';
-                }
-                print '">';
-                print '<a href="contact.php">Contact</a>';
-                print '</li>';
-        
-                ?>
-            </ol>
-        </nav>
+    </head>
+    <!-- ################ body section ######################### -->
+
+    <?php
+    print '<body id="' . $path_parts['filename'] . '">';
+    include 'Fheader.php';
+    include 'Fnav.php';
+    if ($debug) {
+        print '<p>DEBUG MODE IS ON</p>';
+    }
+    ?>

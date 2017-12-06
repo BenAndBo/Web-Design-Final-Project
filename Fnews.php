504f09968c462199ee3053265c8cc27d010347e2
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-12-06 17:39:32 -0500

    Update Fnews.php

--- Fnews.php
+++ Fnews.php
@@ -1,6 +1,29 @@
 <?php
-include 'top.php';
+include 'Ftop.php';
 ?>
+<body>
+    <article>
+        <ul id="news">
+            <li>12/5- Much to all of our dismay, today has been a rainy tuesday for all of Vermont. We hold on hope that maybe overnight there could be some snow showers on the mountains, but the week is forecasted to stay on the warm side. Keep a lookout for the temperatures to drop as we get further into december.</li>
+             
+            <li>12/2- Today was a day for the early bird at Sugarbush. The day started out with beautiful snow, which stayed relatively the same at the top, but the midday sun iced up the lower parts of the mountain a little bit. Expect conditions to remain mostly the same until the next natural snowfall. </li>
+          
+            <li>12/1- A warm week of 40 degree weather left most of the mountains without any new snowfall, but snowmaking teams at Stowe, Killington, Sugarbush, and Jay have been hard at work keeping the trails snow-covered, and getting more trails ready for the season.</li>
+            
+            <li>11/30- Conditions are fairly firm, as the wind has been ripping across the mountains these past few days, making it difficult to blow snow onto the trail. Much of the natural snow has been blown off trails exposing a hard icy base. With that being said, the sides of the trails could hold some sneaky little powder stashes for the true powder-hound to find. </li>
+            
+            <li>11/24- Killington has been open for more than a week now, but this weekend is definitely not the one to book your ski trip. While the snow is perfect hard packed corduroy, tomorrow Killington hosts the women's F.I.S. World Cup, with the Giant Slalom taking place tomorrow followed by a slalom on Sunday. Crowds are expected to approach 20,000 for the event, as everyone wants to come watch racing phenom Mikaela Shiffrin snag the W in her home state. Guest skiing will be heavily limited but head over to the finish of the race and watch some amazing skiers (who knows, you might even learn a thing or two). </li>
+            
+            <li>11/20- POWDER DAY!!!! After receiving a few inches of natural snow overnight and a solid helping of man-made snow, we have a powder day on Stowe’s third day open! Trails are partially groomed or packed powder, while edges of the trail hold some nice shots of deep powder. The snow is supposed to continue throughout the day, so get ready for some free refills after every run. </li>
+            
+            <li>11/17- Stowe Mountain Resort has officially opened!. While it is only their usual first trails open (Sunrise, Lord, Centerline, North Slope, Standard), conditions are beautiful corduroy top to bottom, with almost no exposed patches or rocks. Even most of the little cliffs and side-hits on the trail edges are sufficiently covered. They also have a perfect early season warm up park, consisting of 6 features.</li>
+            
+            <li>11/10- Skiing has officially returned to Vermont! The 2017 season has kicked off in Killington, whose snowmaking team has been hard at work prepping for a very early opening. Trails are extremely limited, but strap on some race skis and go to town on those couple groomers.</li>
+        </ul>
+        
+        
+</article>
+    
+    </body>
 
-  </body>
 </html>

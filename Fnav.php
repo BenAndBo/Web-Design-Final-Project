1837f1920ace58dd6fa65a329c487c91e192593b
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-12-07 21:17:54 -0500

    Update Fnav.php

--- Fnav.php
+++ Fnav.php
@@ -1,57 +1,56 @@
 <nav>
-    <ol>
-       <?php
-       print '<li class="';
-       if($path_parts['filename'] == 'index'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="index.php">Home</a>';
-       print '</li>';
-    
-       print '<li class="';
-       if($path_parts['filename'] == 'form'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="form.php">Form</a>';
-       print '</li>'; 
-       
-       print '<li class="';
-       if($path_parts['filename'] == 'art'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="art.php">Art</a>';
-       print '</li>';
-       
-        print '<li class="';
-       if($path_parts['filename'] == 'contest'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="contest.php">Contest</a>';
-       print '</li>';
-       
-        print '<li class="';
-       if($path_parts['filename'] == 'news'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="news.php">News</a>';
-       print '</li>';
-       
-        print '<li class="';
-       if($path_parts['filename'] == 'weather'){
-         print 'activePage';
-       }
-       print '">';
-       print '<a href="weather.php">Weather</a>';
-       print '</li>';
-       
-       
-       ?>
-    </ol>
-
-</nav>
+            <ol>
+                <?php
+                print '<li class="';
+                if ($path_parts['filename'] == "home") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="home.php">Home</a>';
+                print '</li>';
+                print '<li class="';
+                if ($path_parts['filename'] == "news") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="Fnews.php">News</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "facts") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="facts.php">Facts</a>';
+                print '</li>';
+                                
+                print '<li class="';
+                if ($path_parts['filename'] == "resorts") {
+                    print ' activePage ';
+                }
+                             
+                print '">';
+                print '<a href="resorts.php">Resorts</a>';
+                print '</li>';
+        
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "media") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="media.php">Media</a>';
+                print '</li>';
+        
+                print '<li class="';
+                if ($path_parts['filename'] == "vote") {
+                    print ' activePage ';
+                }
+                print '">';
+                print '<a href="vote.php">Vote</a>';
+                print '</li>';
+                
+                ?>
+            </ol>
+        </nav>
 

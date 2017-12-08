c76548e21926ee316e2a484d4023267c8a830bd7
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-12-07 21:21:21 -0500

    Update media.php

--- media.php
+++ media.php
@@ -2,19 +2,35 @@
 include 'Ftop.php';
 ?>
         <h2>Vermont Media</h2>
-        <table>
+        <table id="media">
             <tr>
                 <td>
                     <video width="320" height="240" controls>
-                        <source src="media/Skiing 2016-2017.mp4" type="video/mp4">
+                        <source src="Media/Skiing 2016-2017.mp4" type="video/mp4">
                     </video>
-                    <br>A skiing video from VT.
+                    <br>Alex Van Raalte's 2016 Season Edit
                 </td>
                 <td>
                     <iframe src="https://player.vimeo.com/video/117208012" width="320" height="240" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                     <br>Bush League: Second Inning from Sugarbush Parks
                 </td>
             </tr>
+            <tr>
+                <td>
+                    <iframe src="https://player.vimeo.com/video/160950213" width="320" height="240" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <br>An edit by Tucker Beatty
+                </td>
+                <td>
+                    <iframe src="https://player.vimeo.com/video/160950213" width="320" height="240" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <br>Another edit by Tucker Beatty
+                </td>
+            </tr>
         </table>
     </body>
+    
+     <?php
+         include ('Ffooter.php');
+    ?>
+    
 </html>
+

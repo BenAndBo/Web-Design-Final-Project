f5b6e31a49d099ed17f7f67cd3cbbbdbc29a7a52
BenAndBo <33698241+BenAndBo@users.noreply.github.com>
2017-12-07 19:48:00 -0500

    Update conditions.php

--- conditions.php
+++ conditions.php
@@ -1,6 +1,62 @@
 <?php
-include 'top.php';
+include 'Ftop.php';
 ?>
 
-  </body>
+        <h2>Notable Moments</h2>
+        <table id="factsTable">
+            <tr>
+                <td>
+                    In 1980, Stratton Resort in Vermont became the first mountain to allow snowboarding. Although snowboarding was born in the 1960's, snowboarders were not allowed on the mountain.
+                </td>
+                <td>
+                    In 1983, the first Burton US Open was held at a small mountain in Vermont called Suicide Six. Originally called the National Snowboarding Championships, the event went on to become one of the most historic events in snowboarding.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    The contest moved to Stratton, VT in 1985 and officially became the US Open Snowboarding Championships. Stratton would host the US Open for the next three decades.
+                </td>
+                <td>
+                    The US Open grew significantly in 1986 with over 200 competitors showing up to compete. Now a days the event is much more restricted as to who can compete and riders must qualify for an invite.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    The most legendary name in snowboarding, Terje Haakonsen made his debut at the 1990 US Open in halfpipe where he fell to fellow legend, Craig Kelly.
+                </td>
+                <td>
+                    In 2000 and 2001, Mount Snow, VT hosted the Winter X Games. The Winter X Games is the biggest stage in skiing and snowboarding. It is the most important contest of the year and is the most desirable to win. Mount Snow attracted hundreds of viewers while hosting the event and some of the biggest names in skiing and snowboarding participated.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    In 2009, Mount Snow hosted a stop on the Winter Dew Tour. The Winter Dew Tour is put on by Mtn Dew and is a highly watched event. Some of the top professionals competed in the event.
+                </td>
+                <td>
+                    Killington Resort in Vermont hosted the Winter Dew Tour stop in 2010 and 2011. The stop was moved from Mount Snow. Many spectators arrived to see the worlds best compete in both skiing and snowboarding.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    By the 1940-1941 season, the longest chair lift in the world had been constructed at Stowe, stretching a full mile up the slope. Although one cannot be certain, based on this fact, people dub Stowe to be the first legit ski resort in the US.
+                </td>
+                <td>
+                    In 2000, the superpipe was brought into play at the US Open stretching 300 feet long with 15 foot walls. Prior to the superpipe, halfpipes had much smaller walls and were much shorter in length. They were also dug by hand.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    In 2002, Mount Snow local, Kelly Clark took home the Olympic Gold Medal for Women's Snowboarding Halfpipe.
+                </td>
+                <td>
+                    Mikaela Shiffrin, one of the most well renowned names in women's ski racing, attended Burke Mountain Academy in Vermont. Shiffrin has gone on to win the Gold Medal in the 2014 Winter Olympics in Sochi, Russia for Woman's Slalom skiing. Shiffrin has also won multiple World Cups and is a US hopeful for the upcoming Olympics games in Korea.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Stowe, VT local, Ty Walker, competed in the 2014 Winter Olympic Games in Sochi, Russia. Walker is a part of the US snowboarding team. She brought the stoke to her home town during the games, however, she got injured in the competition and was unable to perform to her potential. Walker is a 2018 Winter Olympic hopeful.
+                </td>
+            </tr>
+        </table>
+    </body>
 </html>
